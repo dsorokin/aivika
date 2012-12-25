f8829a367306115da09d8d11bffdf30ca96092a3
David Sorokin <david.sorokin@gmail.com>
2012-12-25 09:50:54 +0400

    Two new example where the differential equations are written recursively.

---
+++ examples/ChemicalReactionRec.hs
@@ -0,0 +1,22 @@
+
+{-# LANGUAGE DoRec #-}
+
+import Simulation.Aivika.Dynamics
+import Simulation.Aivika.Dynamics.Simulation
+import Simulation.Aivika.Dynamics.SystemDynamics
+
+specs = Specs { spcStartTime = 0, 
+                spcStopTime = 13, 
+                spcDT = 0.01,
+                spcMethod = RungeKutta4 }
+
+model :: Simulation [Double]
+model = 
+  do rec a <- integ (- ka * a) 100
+         b <- integ (ka * a - kb * b) 0
+         c <- integ (kb * b) 0
+         let ka = 1
+             kb = 1
+     runDynamicsInStopTime $ sequence [a, b, c]
+
+main = runSimulation model specs >>= print
