f39bc1e52e13a79ddc7b2e81dbae281102879c70
David Sorokin <david.sorokin@gmail.com>
2013-07-25 13:24:28 +0400

    [It is still broken] The revised Ref module.

---
+++ Simulation/Aivika/Ref.hs
@@ -0,0 +1,71 @@
+
+-- |
+-- Module     : Simulation.Aivika.Ref
+-- Copyright  : Copyright (c) 2009-2013, David Sorokin <david.sorokin@gmail.com>
+-- License    : BSD3
+-- Maintainer : David Sorokin <david.sorokin@gmail.com>
+-- Stability  : experimental
+-- Tested with: GHC 7.6.3
+--
+-- This module defines an updatable reference that depends on the event queue.
+--
+module Simulation.Aivika.Ref
+       (Ref,
+        refChanged,
+        refChanged_,
+        newRef,
+        readRef,
+        writeRef,
+        modifyRef) where
+
+import Data.IORef
+import Control.Monad
+import Control.Monad.Trans
+
+import Simulation.Aivika.Internal.Simulation
+import Simulation.Aivika.Internal.Event
+import Simulation.Aivika.Internal.Signal
+import Simulation.Aivika.Signal
+
+-- | The 'Ref' type represents a mutable variable similar to the 'IORef' variable 
+-- but only managed by the event queue, which makes the variable value synchronized
+-- with the model.
+data Ref a = 
+  Ref { refValue :: IORef a, 
+        refChangedSource :: SignalSource a }
+
+-- | Create a new reference bound up with the event queue.
+newRef :: a -> Simulation (Ref a)
+newRef a =
+  do x <- liftIO $ newIORef a
+     s <- newSignalSource
+     return Ref { refValue = x, 
+                  refChangedSource = s }
+     
+-- | Read the value of a reference, forcing the bound event queue to raise 
+-- the events in case of need.
+readRef :: Ref a -> Event a
+readRef r = Event $ \p -> readIORef (refValue r)
+
+-- | Write a new value into the reference.
+writeRef :: Ref a -> a -> Event ()
+writeRef r a = Event $ \p -> 
+  do a `seq` writeIORef (refValue r) a
+     invokeEvent p $ triggerSignal (refChangedSource r) a
+
+-- | Mutate the contents of the reference, forcing the bound event queue to
+-- raise all pending events in case of need.
+modifyRef :: Ref a -> (a -> a) -> Event ()
+modifyRef r f = Event $ \p -> 
+  do a <- readIORef (refValue r)
+     let b = f a
+     b `seq` writeIORef (refValue r) b
+     invokeEvent p $ triggerSignal (refChangedSource r) b
+
+-- | Return a signal that notifies about every change of the reference state.
+refChanged :: Ref a -> Signal a
+refChanged v = publishSignal (refChangedSource v)
+
+-- | Return a signal that notifies about every change of the reference state.
+refChanged_ :: Ref a -> Signal ()
+refChanged_ r = mapSignal (const ()) $ refChanged r
