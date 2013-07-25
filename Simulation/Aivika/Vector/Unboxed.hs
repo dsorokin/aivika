1e75d8c11d517f9cac2046cc29a1a5716c8fe6a6
David Sorokin <david.sorokin@gmail.com>
2013-07-25 19:56:13 +0400

    [It is still broken] UVector -> Vector.Unboxed

--- Simulation/Aivika/Vector/Unboxed.hs
+++ Simulation/Aivika/Vector/Unboxed.hs
@@ -1,8 +1,6 @@
 
-{-# LANGUAGE FlexibleContexts #-}
-
 -- |
--- Module     : Simulation.Aivika.UVector
+-- Module     : Simulation.Aivika.Vector.Unboxed
 -- Copyright  : Copyright (c) 2009-2013, David Sorokin <david.sorokin@gmail.com>
 -- License    : BSD3
 -- Maintainer : David Sorokin <david.sorokin@gmail.com>
@@ -11,8 +9,8 @@
 --
 -- An imperative unboxed vector.
 --
-module Simulation.Aivika.UVector
-       (UVector, 
+module Simulation.Aivika.Vector.Unboxed
+       (Vector, 
         newVector, 
         copyVector, 
         vectorCount, 
@@ -31,47 +29,49 @@ import Data.Array.IO.Safe
 import Data.IORef
 import Control.Monad
 
+import Simulation.Aivika.Unboxed
+
 -- | Represents an unboxed resizable vector.
-data UVector a = UVector { vectorArrayRef :: IORef (IOUArray Int a),
-                           vectorCountRef :: IORef Int, 
-                           vectorCapacityRef :: IORef Int }
+data Vector a = Vector { vectorArrayRef :: IORef (IOUArray Int a),
+                         vectorCountRef :: IORef Int, 
+                         vectorCapacityRef :: IORef Int }
 
 -- | Create a new vector.
-newVector :: MArray IOUArray a IO => IO (UVector a)
+newVector :: Unboxed a => IO (Vector a)
 newVector = 
-  do array <- newArray_ (0, 4 - 1)
+  do array <- newUnboxedArray_ (0, 4 - 1)
      arrayRef <- newIORef array
      countRef <- newIORef 0
      capacityRef <- newIORef 4
-     return UVector { vectorArrayRef = arrayRef,
-                      vectorCountRef = countRef,
-                      vectorCapacityRef = capacityRef }
+     return Vector { vectorArrayRef = arrayRef,
+                     vectorCountRef = countRef,
+                     vectorCapacityRef = capacityRef }
 
 -- | Copy the vector.
-copyVector :: (MArray IOUArray a IO) => UVector a -> IO (UVector a)
+copyVector :: Unboxed a => Vector a -> IO (Vector a)
 copyVector vector =
   do array <- readIORef (vectorArrayRef vector)
      count <- readIORef (vectorCountRef vector)
-     array' <- newArray_ (0, count - 1)
+     array' <- newUnboxedArray_ (0, count - 1)
      arrayRef' <- newIORef array'
      countRef' <- newIORef count
      capacityRef' <- newIORef count
      forM_ [0 .. count - 1] $ \i ->
        do x <- readArray array i
           writeArray array' i x
-     return UVector { vectorArrayRef = arrayRef',
-                      vectorCountRef = countRef',
-                      vectorCapacityRef = capacityRef' }
+     return Vector { vectorArrayRef = arrayRef',
+                     vectorCountRef = countRef',
+                     vectorCapacityRef = capacityRef' }
 
 -- | Ensure that the vector has the specified capacity.
-vectorEnsureCapacity :: MArray IOUArray a IO => UVector a -> Int -> IO ()
+vectorEnsureCapacity :: Unboxed a => Vector a -> Int -> IO ()
 vectorEnsureCapacity vector capacity =
   do capacity' <- readIORef (vectorCapacityRef vector)
      when (capacity' < capacity) $
        do array' <- readIORef (vectorArrayRef vector)
           count' <- readIORef (vectorCountRef vector)
           let capacity'' = max (2 * capacity') capacity
-          array'' <- newArray_ (0, capacity'' - 1)
+          array'' <- newUnboxedArray_ (0, capacity'' - 1)
           forM_ [0 .. count' - 1] $ \i ->
             do x <- readArray array' i
                writeArray array'' i x
@@ -79,11 +79,11 @@ vectorEnsureCapacity vector capacity =
           writeIORef (vectorCapacityRef vector) capacity''
           
 -- | Return the element count.
-vectorCount :: MArray IOUArray a IO => UVector a -> IO Int
+vectorCount :: Unboxed a => Vector a -> IO Int
 vectorCount vector = readIORef (vectorCountRef vector)
           
 -- | Add the specified element to the end of the vector.
-appendVector :: MArray IOUArray a IO => UVector a -> a -> IO ()          
+appendVector :: Unboxed a => Vector a -> a -> IO ()          
 appendVector vector item =
   do count <- readIORef (vectorCountRef vector)
      vectorEnsureCapacity vector (count + 1)
@@ -92,19 +92,18 @@ appendVector vector item =
      writeIORef (vectorCountRef vector) (count + 1)
      
 -- | Read a value from the vector, where indices are started from 0.
-readVector :: MArray IOUArray a IO => UVector a -> Int -> IO a
+readVector :: Unboxed a => Vector a -> Int -> IO a
 readVector vector index =
   do array <- readIORef (vectorArrayRef vector)
      readArray array index
           
 -- | Set an array item at the specified index which is started from 0.
-writeVector :: MArray IOUArray a IO => UVector a -> Int -> a -> IO ()
+writeVector :: Unboxed a => Vector a -> Int -> a -> IO ()
 writeVector vector index item =
   do array <- readIORef (vectorArrayRef vector)
      writeArray array index item
           
-vectorBinarySearch' :: (MArray IOUArray a IO, Ord a) => 
-                      IOUArray Int a -> a -> Int -> Int -> IO Int
+vectorBinarySearch' :: (Unboxed a, Ord a) => IOUArray Int a -> a -> Int -> Int -> IO Int
 vectorBinarySearch' array item left right =
   if left > right 
   then return $ - (right + 1) - 1
@@ -119,21 +118,20 @@ vectorBinarySearch' array item left right =
                    
 -- | Return the index of the specified element using binary search; otherwise, 
 -- a negated insertion index minus one: 0 -> -0 - 1, ..., i -> -i - 1, ....
-vectorBinarySearch :: (MArray IOUArray a IO, Ord a) => UVector a -> a -> IO Int
+vectorBinarySearch :: (Unboxed a, Ord a) => Vector a -> a -> IO Int
 vectorBinarySearch vector item =
   do array <- readIORef (vectorArrayRef vector)
      count <- readIORef (vectorCountRef vector)
      vectorBinarySearch' array item 0 (count - 1)
 
-freezeVector :: (MArray IOUArray a IO) => UVector a -> IO (Array Int a)
+freezeVector :: Unboxed a => Vector a -> IO (Array Int a)
 freezeVector vector = 
   do vector' <- copyVector vector
      array   <- readIORef (vectorArrayRef vector')
      freeze array
      
-     
 -- | Insert the element in the vector at the specified index.
-vectorInsert :: (MArray IOUArray a IO) => UVector a -> Int -> a -> IO ()          
+vectorInsert :: Unboxed a => Vector a -> Int -> a -> IO ()          
 vectorInsert vector index item =
   do count <- readIORef (vectorCountRef vector)
      when (index < 0) $
@@ -153,7 +151,7 @@ vectorInsert vector index item =
      writeIORef (vectorCountRef vector) (count + 1)
      
 -- | Delete the element at the specified index.
-vectorDeleteAt :: (MArray IOUArray a IO) => UVector a -> Int -> IO ()
+vectorDeleteAt :: Unboxed a => Vector a -> Int -> IO ()
 vectorDeleteAt vector index =
   do count <- readIORef (vectorCountRef vector)
      when (index < 0) $
@@ -172,8 +170,7 @@ vectorDeleteAt vector index =
      writeIORef (vectorCountRef vector) (count - 1)
      
 -- | Return the index of the item or -1.     
-vectorIndex :: (MArray IOUArray a IO, Eq a) => 
-               UVector a -> a -> IO Int
+vectorIndex :: (Unboxed a, Eq a) => Vector a -> a -> IO Int
 vectorIndex vector item =
   do count <- readIORef (vectorCountRef vector)
      array <- readIORef (vectorArrayRef vector)
