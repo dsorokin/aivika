408bd31d3af3ea8898441b9990bc1301cbcfcd10
David Sorokin <david.sorokin@gmail.com>
2013-07-25 20:29:37 +0400

    [It is still broken] Revised Dynamics.Random module.

--- Simulation/Aivika/Dynamics/Random.hs
+++ Simulation/Aivika/Dynamics/Random.hs
@@ -7,74 +7,38 @@
 -- Stability  : experimental
 -- Tested with: GHC 7.6.3
 --
--- Below are defined random functions that mostly return discrete processes. 
--- Literally, it means that the values are initially defined in integration 
--- time points and then they are passed to the 'discrete' function.
+-- Below are defined random functions that return the 'Dynamics' computations. 
+-- The values are initially defined in the integration time points and then
+-- they are passed in to the 'memo0Dynamics' function to memoize and then interpolate.
 --
 
 module Simulation.Aivika.Dynamics.Random 
-       (newRandom, newNormal, normalGen) where
+       (newRandomDynamics, newNormalDynamics) where
 
 import System.Random
 import Data.IORef
 import Control.Monad.Trans
 
+import Simulation.Aivika.Simulation
+import Simulation.Aivika.Random
 import Simulation.Aivika.Dynamics
-import Simulation.Aivika.Dynamics.Simulation
-import Simulation.Aivika.Dynamics.Base
+import Simulation.Aivika.Dynamics.Memo.Unboxed
 
 -- | Return the uniform random numbers in the integration time points.
-newRandom :: Dynamics Double     -- ^ minimum
-             -> Dynamics Double  -- ^ maximum
-             -> Simulation (Dynamics Double)
-newRandom min max =
-  umemo0 $ do
+newRandomDynamics :: Dynamics Double     -- ^ minimum
+                  -> Dynamics Double  -- ^ maximum
+                  -> Simulation (Dynamics Double)
+newRandomDynamics min max =
+  memo0Dynamics $ do
     x <- liftIO $ getStdRandom random
     min + return x * (max - min)
      
 -- | Return the normal random numbers in the integration time points.
-newNormal :: Dynamics Double     -- ^ mean
-             -> Dynamics Double  -- ^ variance
-             -> Simulation (Dynamics Double)
-newNormal mu nu =
-  do g <- liftIO normalGen
-     umemo0 $ do
+newNormalDynamics :: Dynamics Double     -- ^ mean
+                  -> Dynamics Double  -- ^ variance
+                  -> Simulation (Dynamics Double)
+newNormalDynamics mu nu =
+  do g <- liftIO newNormalGen
+     memo0Dynamics $ do
        x <- liftIO g
        mu + return x * nu
-
--- | Normal random number generator with mean 0 and variance 1.
-normalGen :: IO (IO Double)
-normalGen =
-  do nextRef <- newIORef 0.0
-     flagRef <- newIORef False
-     xi1Ref  <- newIORef 0.0
-     xi2Ref  <- newIORef 0.0
-     psiRef  <- newIORef 0.0
-     let loop =
-           do psi <- readIORef psiRef
-              if (psi >= 1.0) || (psi == 0.0)
-                then do g1 <- getStdRandom random
-                        g2 <- getStdRandom random
-                        let xi1 = 2.0 * g1 - 1.0
-                            xi2 = 2.0 * g2 - 1.0
-                            psi = xi1 * xi1 + xi2 * xi2
-                        writeIORef xi1Ref xi1
-                        writeIORef xi2Ref xi2
-                        writeIORef psiRef psi
-                        loop
-                else writeIORef psiRef $ sqrt (- 2.0 * log psi / psi)
-     return $
-       do flag <- readIORef flagRef
-          if flag
-            then do writeIORef flagRef False
-                    readIORef nextRef
-            else do writeIORef xi1Ref 0.0
-                    writeIORef xi2Ref 0.0
-                    writeIORef psiRef 0.0
-                    loop
-                    xi1 <- readIORef xi1Ref
-                    xi2 <- readIORef xi2Ref
-                    psi <- readIORef psiRef
-                    writeIORef flagRef True
-                    writeIORef nextRef $ xi2 * psi
-                    return $ xi1 * psi
