b41a6a53dad84684a12f827bb8eee59d174243d8
David Sorokin <david.sorokin@gmail.com>
2013-07-29 21:21:10 +0400

    Added internal tests.

---
+++ tests/ProcessParallel.hs
@@ -0,0 +1,68 @@
+
+import Control.Monad
+import Control.Monad.Trans
+
+import Simulation.Aivika.Specs
+import Simulation.Aivika.Simulation
+import Simulation.Aivika.Dynamics
+import Simulation.Aivika.Event
+import Simulation.Aivika.Ref
+import Simulation.Aivika.QueueStrategy
+import Simulation.Aivika.Queue
+import Simulation.Aivika.Resource
+import Simulation.Aivika.Process
+
+specs = Specs 0 1 0.1 RungeKutta4
+
+n1 = 3
+n2 = 5
+
+model :: Simulation ()
+model =
+  do q <- newQueue FCFS StaticPriorities FCFS n2
+
+     pid <- newProcessIdWithCatch
+
+     let timer :: Int -> Int -> Process Int
+         timer i n =
+           do t'  <- liftDynamics time
+              dt' <- liftDynamics dt
+              liftIO $
+                do putStr "Current nested process (number "
+                   putStr $ show i
+                   putStr ", cycle "
+                   putStr $ show n
+                   putStr "): time = "
+                   putStrLn $ show t'
+              holdProcess dt'
+              when ((i == 3) && (n == 1)) $
+                do pid <- processId
+                   --
+                   -- N.B. uncomment this to cancel all processes immediately
+                   -- liftEvent $ cancelProcess pid
+                   --
+                   -- N.B. or, uncomment this to raise the IO exception
+                   -- liftIO $ ioError $ userError "Test error"
+                   --
+                   return ()
+              if (n < n1)
+                then timer i (n + 1)
+                else return i
+
+     runProcessInStartTime IncludingCurrentEvents pid $
+       do let m1 =
+                do xs <- processParallel $ map (\i -> timer i 0) [1..n2]
+                   liftIO $
+                     do putStr $ "The result after all nested processes completed is "
+                        putStrLn $ show xs
+              m2 =
+                liftIO $ putStrLn "Finalization"
+              m3 e =
+                liftIO $ putStrLn "An error has happened"
+          finallyProcess
+            (catchProcess m1 m3)
+            m2
+
+     runEventInStopTime IncludingCurrentEvents $ return ()
+
+main = runSimulation model specs >>= print
--- tests/Queue.hs
+++ tests/Queue.hs
@@ -1,5 +1,4 @@
 
-import System.Random
 import Control.Monad
 import Control.Monad.Trans
 
@@ -13,18 +12,7 @@ import Simulation.Aivika.Queue
 import Simulation.Aivika.Resource
 import Simulation.Aivika.Process
 
-upRate = 1.0 / 1.0       -- reciprocal of mean up time
-repairRate = 1.0 / 0.5   -- reciprocal of mean repair time
-
-specs = Specs { spcStartTime = 0.0,
-                spcStopTime = 1000.0,
-                spcDT = 1.0,
-                spcMethod = RungeKutta4 }
-        
-exprnd :: Double -> IO Double
-exprnd lambda =
-  do x <- getStdRandom random
-     return (- log x / lambda)
+specs = Specs 0 1 0.1 RungeKutta4
 
 n1 = 10
 n2 = 3
