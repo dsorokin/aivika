c8be3555ce94868f439dd14d90aef6f182f70e5c
David Sorokin <david.sorokin@gmail.com>
2013-07-30 11:09:32 +0400

    Using more high-level simulation entities in the Furnace model, which simplifies much.

--- examples/Furnace.hs
+++ examples/Furnace.hs
@@ -11,8 +11,11 @@ import Simulation.Aivika.Event
 import Simulation.Aivika.Ref
 import Simulation.Aivika.Process
 import Simulation.Aivika.Random
-
-import qualified Simulation.Aivika.DoubleLinkedList as DLL
+import Simulation.Aivika.QueueStrategy
+import Simulation.Aivika.Queue.Infinite
+import Simulation.Aivika.Signal
+import Simulation.Aivika.Observable
+import Simulation.Aivika.Statistics
 
 -- | The simulation specs.
 specs = Specs { spcStartTime = 0.0,
@@ -42,27 +45,24 @@ data Furnace =
             -- ^ The pits for ingots.
             furnacePitCount :: Ref Int,
             -- ^ The count of active pits with ingots.
-            furnaceAwaitingIngots :: DLL.DoubleLinkedList Ingot,
-            -- ^ The awaiting ingots in the queue.
-            furnaceQueueCount :: Ref Int,
-            -- ^ The queue count.
-            furnaceWaitCount :: Ref Int,
-            -- ^ The count of awaiting ingots.
-            furnaceWaitTime :: Ref Double,
-            -- ^ The wait time for all loaded ingots.
-            furnaceHeatingTime :: Ref Double,
-            -- ^ The heating time for all unloaded ingots.
+            furnaceQueue :: FCFSQueue Ingot,
+            -- ^ The furnace queue.
+            furnaceUnloadedSource :: SignalSource (),
+            -- ^ Notifies when the ingots have been
+            -- unloaded from the furnace.
+            furnaceHeatingTime :: Ref (SamplingStats Double),
+            -- ^ The heating time for the ready ingots.
             furnaceTemp :: Ref Double,
             -- ^ The furnace temperature.
-            furnaceTotalCount :: Ref Int,
-            -- ^ The total count of ingots.
-            furnaceLoadCount :: Ref Int,
-            -- ^ The count of loaded ingots.
-            furnaceUnloadCount :: Ref Int,
-            -- ^ The count of unloaded ingots.
-            furnaceUnloadTemps :: Ref [Double]
-            -- ^ The temperatures of all unloaded ingots.
+            furnaceReadyCount :: Ref Int,
+            -- ^ The count of ready ingots.
+            furnaceReadyTemps :: Ref [Double]
+            -- ^ The temperatures of all ready ingots.
             }
+
+-- | Notifies when the ingots have been unloaded from the furnace.
+furnaceUnloaded :: Furnace -> Signal ()
+furnaceUnloaded = publishSignal . furnaceUnloadedSource
 
 -- | A pit in the furnace to place the ingots.
 data Pit = 
@@ -93,29 +93,21 @@ newFurnace =
   do normalGen <- liftIO newNormalGen
      pits <- sequence [newPit | i <- [1..10]]
      pitCount <- newRef 0
-     awaitingIngots <- liftIO DLL.newList
-     queueCount <- newRef 0
-     waitCount <- newRef 0
-     waitTime <- newRef 0.0
-     heatingTime <- newRef 0.0
+     queue <- newFCFSQueue
+     heatingTime <- newRef emptySamplingStats
      h <- newRef 1650.0
-     totalCount <- newRef 0
-     loadCount <- newRef 0
-     unloadCount <- newRef 0
-     unloadTemps <- newRef []
+     readyCount <- newRef 0
+     readyTemps <- newRef []
+     s <- newSignalSource
      return Furnace { furnaceNormalGen = normalGen,
                       furnacePits = pits,
                       furnacePitCount = pitCount,
-                      furnaceAwaitingIngots = awaitingIngots,
-                      furnaceQueueCount = queueCount,
-                      furnaceWaitCount = waitCount,
-                      furnaceWaitTime = waitTime,
+                      furnaceQueue = queue,
+                      furnaceUnloadedSource = s,
                       furnaceHeatingTime = heatingTime,
                       furnaceTemp = h,
-                      furnaceTotalCount = totalCount,
-                      furnaceLoadCount = loadCount, 
-                      furnaceUnloadCount = unloadCount, 
-                      furnaceUnloadTemps = unloadTemps }
+                      furnaceReadyCount = readyCount, 
+                      furnaceReadyTemps = readyTemps }
 
 -- | Create a new pit.
 newPit :: Simulation Pit
@@ -169,80 +161,69 @@ tryUnloadPit furnace pit =
   do h' <- readRef (pitTemp pit)
      when (h' >= 2000.0) $
        do Just ingot <- readRef (pitIngot pit)  
-          unloadIngot ingot pit
+          unloadIngot furnace ingot pit
 
 -- | Try to load an awaiting ingot in the specified empty pit.
 tryLoadPit :: Furnace -> Pit -> Event ()       
 tryLoadPit furnace pit =
-  do let ingots = furnaceAwaitingIngots furnace
-     flag <- liftIO $ DLL.listNull ingots
-     unless flag $
-       do ingot <- liftIO $ DLL.listFirst ingots
-          liftIO $ DLL.listRemoveFirst ingots
-          t' <- liftDynamics time
-          modifyRef (furnaceQueueCount furnace) (+ (-1))
-          loadIngot (ingot { ingotLoadTime = t',
-                             ingotLoadTemp = 400.0 }) pit
+  do ingot <- tryDequeue (furnaceQueue furnace)
+     case ingot of
+       Nothing ->
+         return ()
+       Just ingot ->
+         do t' <- liftDynamics time
+            loadIngot furnace (ingot { ingotLoadTime = t',
+                                       ingotLoadTemp = 400.0 }) pit
               
 -- | Unload the ingot from the specified pit.       
-unloadIngot :: Ingot -> Pit -> Event ()
-unloadIngot ingot pit = 
+unloadIngot :: Furnace -> Ingot -> Pit -> Event ()
+unloadIngot furnace ingot pit = 
   do h' <- readRef (pitTemp pit)
      writeRef (pitIngot pit) Nothing
      writeRef (pitTemp pit) 0.0
-     
+
      -- count the active pits
-     let furnace = ingotFurnace ingot
-     count <- readRef (furnacePitCount furnace)
-     writeRef (furnacePitCount furnace) (count - 1)
+     modifyRef (furnacePitCount furnace) (+ (- 1))
      
      -- how long did we heat the ingot up?
      t' <- liftDynamics time
-     modifyRef (furnaceHeatingTime furnace)
-       (+ (t' - ingotLoadTime ingot))
+     modifyRef (furnaceHeatingTime furnace) $
+       addSamplingStats (t' - ingotLoadTime ingot)
      
      -- what is the temperature of the unloaded ingot?
-     modifyRef (furnaceUnloadTemps furnace) (h' :)
+     modifyRef (furnaceReadyTemps furnace) (h' :)
      
-     -- count the unloaded ingots
-     modifyRef (furnaceUnloadCount furnace) (+ 1)
+     -- count the ready ingots
+     modifyRef (furnaceReadyCount furnace) (+ 1)
      
 -- | Load the ingot in the specified pit
-loadIngot :: Ingot -> Pit -> Event ()
-loadIngot ingot pit =
+loadIngot :: Furnace -> Ingot -> Pit -> Event ()
+loadIngot furnace ingot pit =
   do writeRef (pitIngot pit) $ Just ingot
      writeRef (pitTemp pit) $ ingotLoadTemp ingot
-     
+
      -- count the active pits
-     let furnace = ingotFurnace ingot
+     modifyRef (furnacePitCount furnace) (+ 1)
      count <- readRef (furnacePitCount furnace)
-     writeRef (furnacePitCount furnace) (count + 1)
      
      -- decrease the furnace temperature
      h <- readRef (furnaceTemp furnace)
      let h' = ingotLoadTemp ingot
-         dh = - (h - h') / fromInteger (toInteger (count + 1))
+         dh = - (h - h') / fromIntegral count
      writeRef (furnaceTemp furnace) $ h + dh
-
-     -- how long did we keep the ingot in the queue?
-     t' <- liftDynamics time
-     modifyRef (furnaceWaitCount furnace) (+ 1) 
-     modifyRef (furnaceWaitTime furnace)
-       (+ (t' - ingotReceiveTime ingot))
-
-     -- count the loaded ingots
-     modifyRef (furnaceLoadCount furnace) (+ 1)
-  
+ 
 -- | Start iterating the furnace processing through the event queue.
 startIteratingFurnace :: Furnace -> Event ()
 startIteratingFurnace furnace = 
   let pits = furnacePits furnace
   in enqueueEventWithIntegTimes $
-     do ready <- ingotsReady furnace
+     do -- try to unload ready ingots
+        ready <- ingotsReady furnace
         when ready $ 
           do mapM_ (tryUnloadPit furnace) pits
-             pits' <- emptyPits furnace
-             mapM_ (tryLoadPit furnace) pits'
+             triggerSignal (furnaceUnloadedSource furnace) ()
+
+        -- heat up
         mapM_ heatPitUp pits
         
         -- update the temperature of the furnace
@@ -257,32 +238,36 @@ emptyPits furnace =
   filterM (fmap isNothing . readRef . pitIngot) $
   furnacePits furnace
 
--- | Accept a new ingot.
-acceptIngot :: Furnace -> Event ()
-acceptIngot furnace =
-  do ingot <- newIngot furnace
-     
-     -- counting
-     modifyRef (furnaceTotalCount furnace) (+ 1)
-     
-     -- check what to do with the new ingot
-     count <- readRef (furnacePitCount furnace)
-     if count >= 10
-       then do let ingots = furnaceAwaitingIngots furnace
-               liftIO $ DLL.listAddLast ingots ingot
-               modifyRef (furnaceQueueCount furnace) (+ 1)
-       else do pit:_ <- emptyPits furnace
-               loadIngot ingot pit
-       
--- | Process the furnace.
-processFurnace :: Furnace -> Process ()
-processFurnace furnace =
+-- | This process takes ingots from the queue and then
+-- loads them in the furnace.
+loadingProcess :: Furnace -> Process ()
+loadingProcess furnace =
+  do ingot <- dequeue (furnaceQueue furnace)
+     let wait :: Process ()
+         wait =
+           do count <- liftEvent $ readRef (furnacePitCount furnace)
+              when (count >= 10) $
+                do awaitSignal (furnaceUnloaded furnace)
+                   wait
+     wait
+     --  take any empty pit and load it
+     liftEvent $
+       do pit: _ <- emptyPits furnace
+          loadIngot furnace ingot pit
+     -- repeat it again
+     loadingProcess furnace
+                  
+-- | The input process that adds new ingots to the queue.
+inputProcess :: Furnace -> Process ()
+inputProcess furnace =
   do delay <- liftIO $ exprnd (1.0 / 2.5)
      holdProcess delay
      -- we have got a new ingot
-     liftEvent $ acceptIngot furnace
+     liftEvent $
+       do ingot <- newIngot furnace
+          enqueue (furnaceQueue furnace) ingot
      -- repeat it again
-     processFurnace furnace
+     inputProcess furnace
 
 -- | Initialize the furnace.
 initializeFurnace :: Furnace -> Event ()
@@ -295,97 +280,84 @@ initializeFurnace furnace =
      x6 <- newIngot furnace
      let p1 : p2 : p3 : p4 : p5 : p6 : ps = 
            furnacePits furnace
-     loadIngot (x1 { ingotLoadTemp = 550.0 }) p1
-     loadIngot (x2 { ingotLoadTemp = 600.0 }) p2
-     loadIngot (x3 { ingotLoadTemp = 650.0 }) p3
-     loadIngot (x4 { ingotLoadTemp = 700.0 }) p4
-     loadIngot (x5 { ingotLoadTemp = 750.0 }) p5
-     loadIngot (x6 { ingotLoadTemp = 800.0 }) p6
-     writeRef (furnaceTotalCount furnace) 6
+     loadIngot furnace (x1 { ingotLoadTemp = 550.0 }) p1
+     loadIngot furnace (x2 { ingotLoadTemp = 600.0 }) p2
+     loadIngot furnace (x3 { ingotLoadTemp = 650.0 }) p3
+     loadIngot furnace (x4 { ingotLoadTemp = 700.0 }) p4
+     loadIngot furnace (x5 { ingotLoadTemp = 750.0 }) p5
+     loadIngot furnace (x6 { ingotLoadTemp = 800.0 }) p6
      writeRef (furnaceTemp furnace) 1650.0
      
--- | Return a count, average and deviation.
-stats :: [Double] -> (Int, Double, Double)
-stats xs = (length xs, ex, sx)
-  where
-    n  = fromInteger $ toInteger $ length xs
-    ex = sum xs / n
-    dx = (sum . map rho) xs / (n - 1.0)
-    sx = sqrt dx
-    rho x = (x - ex) ^ 2
-
 -- | The simulation model.
 model :: Simulation ()
 model =
   do furnace <- newFurnace
-     pid <- newProcessId
-
+     pid1 <- newProcessId
+     pid2 <- newProcessId
+  
      -- initialize the furnace and start its iterating in start time
      runEventInStartTime IncludingCurrentEvents $
        do initializeFurnace furnace
           startIteratingFurnace furnace
      
-     -- accept input ingots
+     -- generate randomly new input ingots
      runProcessInStartTime IncludingCurrentEvents
-       pid (processFurnace furnace)
+       pid1 (inputProcess furnace)
+
+     -- load permanently the input ingots in the furnace
+     runProcessInStartTime IncludingCurrentEvents
+       pid2 (loadingProcess furnace)
      
      -- run the model in the final time point
      runEventInStopTime IncludingCurrentEvents $
        do -- the ingots
-          c0 <- readRef (furnaceTotalCount furnace)
-          c1 <- readRef (furnaceLoadCount furnace)
-          c2 <- readRef (furnaceUnloadCount furnace)
-          c3 <- readRef (furnaceWaitCount furnace)
+          c0 <- readObservable $ queueStoreCount (furnaceQueue furnace)
+          c1 <- readObservable $ queueOutputCount (furnaceQueue furnace)
+          c2 <- readRef (furnaceReadyCount furnace)
               
           liftIO $ do
             putStrLn "The count of ingots:"
             putStrLn $ "  total  = " ++ show c0
             putStrLn $ "  loaded = " ++ show c1
             putStrLn $ "  ready  = " ++ show c2
-            putStrLn $ "  awaited in the queue = " ++ show c3
             putStrLn ""
          
           -- the temperature of the ready ingots
-          (n1, e1, d1) <- 
-            fmap stats $ readRef (furnaceUnloadTemps furnace)
-                
+          temps <- readRef (furnaceReadyTemps furnace)
+            
           liftIO $ do 
-            putStrLn "The temperature of the ready ingots:"
-            putStrLn $ "  average   = " ++ show e1
-            putStrLn $ "  deviation = " ++ show d1
+            putStrLn "The temperature of ready ingots:"
+            putStrLn $ "  " ++ show (listSamplingStats temps)
+            putStrLn ""
+              
+          -- the mean heating time
+          r5 <- readRef (furnaceHeatingTime furnace)
+            
+          liftIO $ do
+            putStrLn "The heating time:"
+            putStrLn $ "  " ++ show r5
             putStrLn ""
                 
           -- the ingots in pits
           r2 <- readRef (furnacePitCount furnace)
               
           liftIO $ do
-            putStrLn "The ingots in pits (in the final time): "
+            putStr "The ingots in pits (in the final time): "
             putStrLn $ show r2
             putStrLn ""
               
-          -- the queue size
-          r3 <- readRef (furnaceQueueCount furnace)
+          -- the queue size and mean wait time
+          r3 <- readObservable $ queueCount (furnaceQueue furnace)
+          
+          r4 <- fmap samplingStatsMean $
+                readObservable $
+                queueWaitTime (furnaceQueue furnace) 
      
           liftIO $ do
-            putStrLn "The queue size (in the final time): "
-            putStrLn $ show r3
+            putStrLn "The queue: "
+            putStrLn $ "  size (in the final time) = " ++ show r3
+            putStrLn $ "  mean wait time = " ++ show r4
             putStrLn ""
-              
-          -- the mean wait time in the queue
-          waitTime <- readRef (furnaceWaitTime furnace)
-          waitCount <- readRef (furnaceWaitCount furnace)
-
-          let t4 = waitTime / fromIntegral waitCount
-         
-          -- the mean heating time
-          heatingTime <- readRef (furnaceHeatingTime furnace)
-          unloadCount <- readRef (furnaceUnloadCount furnace)
-
-          let t5 = heatingTime / fromIntegral unloadCount
-                    
-          liftIO $ do
-            putStrLn $ "The mean wait time: " ++ show t4
-            putStrLn $ "The mean heating time: " ++ show t5
 
 -- | The main program.
 main = runSimulation model specs
