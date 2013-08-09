a8eec98ed7fd3657f2afe6e2be83fff708234d53
David Sorokin <david.sorokin@gmail.com>
2013-08-09 08:58:33 +0400

    The Worker can have a state now.

--- Simulation/Aivika/Worker.hs
+++ Simulation/Aivika/Worker.hs
@@ -13,7 +13,11 @@ module Simulation.Aivika.Worker
         Worker,
         newWorker,
         newWorkerUsingId,
+        newWorkerWithState,
+        newWorkerWithStateUsingId,
         workerProcessId,
+        workerInitState,
+        workerState,
         -- * Worker's Processor
         workerProcessor,
         -- * Worker Properties and Activities
@@ -48,11 +52,15 @@ import Simulation.Aivika.Processor
 import Simulation.Aivika.Stream
 import Simulation.Aivika.Statistics
 
--- | It models a worker that takes @a@ and produces @b@.
-data Worker a b =
+-- | It models a worker that takes @a@ and produces @b@ having state @s@.
+data Worker s a b =
   Worker { workerProcessId :: ProcessId,
            -- ^ The process identifier of the worker.
-           workerProduce :: a -> Process b,
+           workerInitState :: s,
+           -- ^ The initial state of the worker.
+           workerStateRef :: IORef s,
+           -- ^ The current state of the worker.
+           workerProduce :: (s, a) -> Process (s, b),
            -- ^ Produce @b@ by specified @a@.
            workerTotalFreeTimeRef :: IORef Double,
            -- ^ The counted total free time.
@@ -76,17 +84,51 @@ data Worker a b =
            -- proceeding to the first task.
          }
 
--- | Create a new worker that can produce output @b@ by the input @a@.
-newWorker :: (a -> Process b) -> Simulation (Worker a b)
+-- | Create a new worker that can produce output @b@ by input @a@.
+newWorker :: (a -> Process b)
+             -- ^ produce an output by the specified input
+             -> Simulation (Worker () a b)
 newWorker produce =
   do pid <- newProcessId
      newWorkerUsingId pid produce
 
 -- | Create with the specified identifier a new worker that can produce
--- output @b@ by the input @a@.
-newWorkerUsingId :: ProcessId -> (a -> Process b) -> Simulation (Worker a b)
+-- output @b@ by input @a@.
+newWorkerUsingId :: ProcessId
+                    -- ^ the idenitifier for the internal process
+                    -> (a -> Process b)
+                    -- ^ produce an output by the specified input
+                    -> Simulation (Worker () a b)
 newWorkerUsingId pid produce =
-  do r1 <- liftIO $ newIORef 0
+  newWorkerWithStateUsingId pid () $ \(s, a) ->
+  do b <- produce a
+     return (s, b)
+
+-- | Create a new worker that can produce output @b@ by input @a@
+-- starting from state @s@.
+newWorkerWithState :: s
+                      -- ^ the initial state
+                      -> ((s, a) -> Process (s, b))
+                      -- ^ produce an output by the specified input
+                      -- and update the state 
+                      -> Simulation (Worker s a b)
+newWorkerWithState state produce =
+  do pid <- newProcessId
+     newWorkerWithStateUsingId pid state produce
+
+-- | Create with the specified identifier a new worker that can produce
+-- output @b@ by input @a@ starting from state @s@.
+newWorkerWithStateUsingId :: ProcessId
+                             -- ^ the identifier for the internal process
+                             -> s
+                             -- ^ the initial state
+                             -> ((s, a) -> Process (s, b))
+                             -- ^ produce an output by the specified input
+                             -- and update the state
+                             -> Simulation (Worker s a b)
+newWorkerWithStateUsingId pid state produce =
+  do r0 <- liftIO $ newIORef state
+     r1 <- liftIO $ newIORef 0
      r2 <- liftIO $ newIORef 0
      r3 <- liftIO $ newIORef 0
      r4 <- liftIO $ newIORef emptySamplingStats
@@ -96,6 +138,8 @@ newWorkerUsingId pid produce =
      s2 <- newSignalSource
      s3 <- newSignalSource
      return Worker { workerProcessId = pid,
+                     workerInitState = state,
+                     workerStateRef = r0,
                      workerProduce = produce,
                      workerTotalFreeTimeRef = r1,
                      workerTotalEffortTimeRef = r2,
@@ -113,14 +157,14 @@ newWorkerUsingId pid produce =
 -- with the process identifier, which is either specified explicitly or generated
 -- when creating a worker. This identifier cannot be used twice when running
 -- the processor; otherwise, a run time error will be raised.
-workerProcessor :: Worker a b -> Processor a b
+workerProcessor :: Worker s a b -> Processor a b
 workerProcessor w =
   Processor $ \xs ->
   Cons $
   do loadingRes   <- liftSimulation $ newFCFSResourceWithMaxCount 1 (Just 1)
      producingRes <- liftSimulation $ newFCFSResourceWithMaxCount 0 (Just 1)
      production   <- liftIO $ newIORef Nothing
-     let worker t' xs =
+     let worker s t' xs =
            do requestResource loadingRes
               t0 <- liftDynamics time
               liftEvent $
@@ -139,17 +183,18 @@ workerProcessor w =
                           addSamplingStats (t1 - t0)
                    triggerSignal (workerLoadedSource w) a
               -- produce
-              b <- workerProduce w a
+              (s', b) <- workerProduce w (s, a)
               t2 <- liftDynamics time
               liftEvent $
                 do liftIO $
                      do writeIORef production (Just b)
+                        writeIORef (workerStateRef w) s'
                         modifyIORef (workerTotalEffortTimeRef w) (+ (t2 - t1))
                         modifyIORef (workerEffortTimeRef w) $
                           addSamplingStats (t2 - t1)
                    releaseResourceWithinEvent producingRes
                    triggerSignal (workerProducedSource w) (a, b)
-              worker t2 xs'
+              worker s' t2 xs'
          writer =
            Cons $
            do requestResource producingRes
@@ -158,14 +203,23 @@ workerProcessor w =
               releaseResource loadingRes
               return (b, writer)
      t' <- liftDynamics time
-     childProcessUsingId (workerProcessId w) $ worker t' xs
+     childProcessUsingId (workerProcessId w) $
+       worker (workerInitState w) t' xs
      runStream writer
+
+-- | Return the current state of the worker.
+workerState :: Worker s a b -> Observable s
+workerState w =
+  let read = Event $ \p -> readIORef (workerStateRef w)
+  in Observable { readObservable = read,
+                  observableChanged_ =
+                    mapSignal (const ()) (workerProduced w) }
 
 -- | Return the counted total free time for the worker.
 --
 -- The value returned changes discretely and it is usually delayed relative
 -- to the current simulation time.
-workerTotalFreeTime :: Worker a b -> Observable Double
+workerTotalFreeTime :: Worker s a b -> Observable Double
 workerTotalFreeTime w =
   let read = Event $ \p -> readIORef (workerTotalFreeTimeRef w)
   in Observable { readObservable = read,
@@ -176,7 +230,7 @@ workerTotalFreeTime w =
 --
 -- The value returned changes discretely and it is usually delayed relative
 -- to the current simulation time.
-workerTotalEffortTime :: Worker a b -> Observable Double
+workerTotalEffortTime :: Worker s a b -> Observable Double
 workerTotalEffortTime w =
   let read = Event $ \p -> readIORef (workerTotalEffortTimeRef w)
   in Observable { readObservable = read,
@@ -187,7 +241,7 @@ workerTotalEffortTime w =
 --
 -- The value returned changes discretely and it is usually delayed relative
 -- to the current simulation time.
-workerTotalTimeInLock :: Worker a b -> Observable Double
+workerTotalTimeInLock :: Worker s a b -> Observable Double
 workerTotalTimeInLock w =
   let read = Event $ \p -> readIORef (workerTotalTimeInLockRef w)
   in Observable { readObservable = read,
@@ -197,7 +251,7 @@ workerTotalTimeInLock w =
 --
 -- The value returned changes discretely and it is usually delayed relative
 -- to the current simulation time.
-workerFreeTime :: Worker a b -> Observable (SamplingStats Double)
+workerFreeTime :: Worker s a b -> Observable (SamplingStats Double)
 workerFreeTime w =
   let read = Event $ \p -> readIORef (workerFreeTimeRef w)
   in Observable { readObservable = read,
@@ -208,7 +262,7 @@ workerFreeTime w =
 --
 -- The value returned changes discretely and it is usually delayed relative
 -- to the current simulation time.
-workerEffortTime :: Worker a b -> Observable (SamplingStats Double)
+workerEffortTime :: Worker s a b -> Observable (SamplingStats Double)
 workerEffortTime w =
   let read = Event $ \p -> readIORef (workerEffortTimeRef w)
   in Observable { readObservable = read,
@@ -219,7 +273,7 @@ workerEffortTime w =
 --
 -- The value returned changes discretely and it is usually delayed relative
 -- to the current simulation time.
-workerTimeInLock :: Worker a b -> Observable (SamplingStats Double)
+workerTimeInLock :: Worker s a b -> Observable (SamplingStats Double)
 workerTimeInLock w =
   let read = Event $ \p -> readIORef (workerTimeInLockRef w)
   in Observable { readObservable = read,
@@ -237,7 +291,7 @@ workerTimeInLock w =
 --
 -- As before in this module, the value returned changes discretely and
 -- it is usually delayed relative to the current simulation time.
-workerFreeTimeFactor :: Worker a b -> Observable Double
+workerFreeTimeFactor :: Worker s a b -> Observable Double
 workerFreeTimeFactor w =
   let read =
         Event $ \p ->
@@ -263,7 +317,7 @@ workerFreeTimeFactor w =
 --
 -- As before in this module, the value returned changes discretely and
 -- it is usually delayed relative to the current simulation time.
-workerEffortTimeFactor :: Worker a b -> Observable Double
+workerEffortTimeFactor :: Worker s a b -> Observable Double
 workerEffortTimeFactor w =
   let read =
         Event $ \p ->
@@ -290,7 +344,7 @@ workerEffortTimeFactor w =
 --
 -- As before in this module, the value returned changes discretely and
 -- it is usually delayed relative to the current simulation time.
-workerTimeInLockFactor :: Worker a b -> Observable Double
+workerTimeInLockFactor :: Worker s a b -> Observable Double
 workerTimeInLockFactor w =
   let read =
         Event $ \p ->
@@ -306,22 +360,22 @@ workerTimeInLockFactor w =
                   observableChanged_ = signal }
 
 -- | Raised when the worker is loaded with a new task.
-workerLoaded :: Worker a b -> Signal a
+workerLoaded :: Worker s a b -> Signal a
 workerLoaded = publishSignal . workerLoadedSource
 
 -- | Raised when the worker has just produced the result.
-workerProduced :: Worker a b -> Signal (a, b)
+workerProduced :: Worker s a b -> Signal (a, b)
 workerProduced = publishSignal . workerProducedSource
 
 -- | Raised when the worker is released either after producing
 -- the result and the further releasing from the lock or in the outset before
 -- proceeding to the first task.
-workerReleased :: Worker a b -> Signal ()
+workerReleased :: Worker s a b -> Signal ()
 workerReleased = publishSignal . workerReleasedSource
 
 -- | Return the summary for the worker with desciption of its
 -- properties and activities using the specified indent.
-workerSummary :: Worker a b -> Int -> Observable ShowS
+workerSummary :: Worker s a b -> Int -> Observable ShowS
 workerSummary w indent =
   let read =
         Event $ \p ->
