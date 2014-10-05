80d6e2e3f88a7066639f36b00a0e884339f6a600
David Sorokin <david.sorokin@gmail.com>
2014-10-05 16:13:05 +0400

    Updated example MachRep1.

--- examples/MachRep1.hs
+++ examples/MachRep1.hs
@@ -28,7 +28,7 @@ specs = Specs { spcStartTime = 0.0,
                 spcMethod = RungeKutta4,
                 spcGeneratorType = SimpleGenerator }
         
-model :: Simulation Double
+model :: Simulation Results
 model =
   do totalUpTime <- newRef 0.0
      
@@ -48,10 +48,20 @@ model =
 
      runProcessInStartTime machine
      runProcessInStartTime machine
-     
-     runEventInStopTime $
-       do x <- readRef totalUpTime
-          y <- liftParameter stoptime
-          return $ x / (2 * y)
+
+     let upTimeProp =
+           do x <- readRef totalUpTime
+              y <- liftParameter stoptime
+              return $ x / (2 * y)
+
+     return $
+       results
+       [resultSource
+        "upTimeProp"
+        "The long-run proportion of up time (~ 0.66)"
+        upTimeProp]
   
-main = runSimulation model specs >>= print
+main =
+  printSimulationResultsInStopTime
+  printResultSourceInEnglish
+  model specs
