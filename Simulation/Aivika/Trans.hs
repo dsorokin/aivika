c543a89d2dc6b65c2171a585ef2377298a92c934
David Sorokin <david.sorokin@gmail.com>
2014-10-12 05:44:00 +0400

    Added module Trans.

---
+++ Simulation/Aivika/Trans.hs
@@ -0,0 +1,82 @@
+
+-- |
+-- Module     : Simulation.Aivika.Trans
+-- Copyright  : Copyright (c) 2009-2014, David Sorokin <david.sorokin@gmail.com>
+-- License    : BSD3
+-- Maintainer : David Sorokin <david.sorokin@gmail.com>
+-- Stability  : experimental
+-- Tested with: GHC 7.8.3
+--
+-- This module re-exports the most part of the library functionality.
+-- But there are modules that must be imported explicitly, though.
+--
+module Simulation.Aivika.Trans
+       (-- * Modules
+        -- module Simulation.Aivika.Trans.Agent,
+        -- module Simulation.Aivika.Trans.Arrival,
+        -- module Simulation.Aivika.Trans.Circuit,
+        module Simulation.Aivika.Trans.Cont,
+        module Simulation.Aivika.Trans.Dynamics,
+        module Simulation.Aivika.Trans.Dynamics.Interpolate,
+        module Simulation.Aivika.Trans.Dynamics.Memo.Unboxed,
+        -- module Simulation.Aivika.Trans.Dynamics.Random,
+        module Simulation.Aivika.Trans.Event,
+        module Simulation.Aivika.Trans.Generator,
+        -- module Simulation.Aivika.Trans.Net,
+        module Simulation.Aivika.Trans.Parameter,
+        -- module Simulation.Aivika.Trans.Parameter.Random,
+        module Simulation.Aivika.Trans.Process,
+        -- module Simulation.Aivika.Trans.Processor,
+        -- module Simulation.Aivika.Trans.Processor.RoundRobbin,
+        -- module Simulation.Aivika.Trans.QueueStrategy,
+        module Simulation.Aivika.Trans.Ref,
+        -- module Simulation.Aivika.Trans.Resource,
+        -- module Simulation.Aivika.Trans.Results,
+        -- module Simulation.Aivika.Trans.Results.Locale,
+        -- module Simulation.Aivika.Trans.Results.IO,
+        -- module Simulation.Aivika.Trans.Server,
+        module Simulation.Aivika.Trans.Signal,
+        module Simulation.Aivika.Trans.Simulation,
+        module Simulation.Aivika.Trans.Specs,
+        module Simulation.Aivika.Trans.Statistics,
+        -- module Simulation.Aivika.Trans.Statistics.Accumulator,
+        -- module Simulation.Aivika.Trans.Stream,
+        -- module Simulation.Aivika.Trans.Stream.Random,
+        -- module Simulation.Aivika.Trans.Task,
+        -- module Simulation.Aivika.Trans.Transform,
+        -- module Simulation.Aivika.Trans.Var.Unboxed) where
+       ) where
+
+-- import Simulation.Aivika.Trans.Agent
+-- import Simulation.Aivika.Trans.Arrival
+-- import Simulation.Aivika.Trans.Circuit
+import Simulation.Aivika.Trans.Cont
+import Simulation.Aivika.Trans.Dynamics
+import Simulation.Aivika.Trans.Dynamics.Interpolate
+import Simulation.Aivika.Trans.Dynamics.Memo.Unboxed
+-- import Simulation.Aivika.Trans.Dynamics.Random
+import Simulation.Aivika.Trans.Event
+import Simulation.Aivika.Trans.Generator
+-- import Simulation.Aivika.Trans.Net
+import Simulation.Aivika.Trans.Parameter
+-- import Simulation.Aivika.Trans.Parameter.Random
+import Simulation.Aivika.Trans.Process
+-- import Simulation.Aivika.Trans.Processor
+-- import Simulation.Aivika.Trans.Processor.RoundRobbin
+-- import Simulation.Aivika.Trans.QueueStrategy
+import Simulation.Aivika.Trans.Ref
+-- import Simulation.Aivika.Trans.Resource
+-- import Simulation.Aivika.Trans.Results
+-- import Simulation.Aivika.Trans.Results.Locale
+-- import Simulation.Aivika.Trans.Results.IO
+-- import Simulation.Aivika.Trans.Server
+import Simulation.Aivika.Trans.Signal
+import Simulation.Aivika.Trans.Simulation
+import Simulation.Aivika.Trans.Specs
+import Simulation.Aivika.Trans.Statistics
+-- import Simulation.Aivika.Trans.Statistics.Accumulator
+-- import Simulation.Aivika.Trans.Stream
+-- import Simulation.Aivika.Trans.Stream.Random
+-- import Simulation.Aivika.Trans.Task
+-- import Simulation.Aivika.Trans.Transform
+-- import Simulation.Aivika.Trans.Var.Unboxed
