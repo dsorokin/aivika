4e6465b86bf869435837bbe187e5683bc488db35
David Sorokin <david.sorokin@gmail.com>
2015-05-27 20:50:18 +0300

    Added the example description.

--- examples/InventorySystem.hs
+++ examples/InventorySystem.hs
@@ -6,6 +6,32 @@
 -- [1] A. Alan B. Pritsker, Simulation with Visual SLAM and AweSim, 2nd ed.
 --
 -- [2] Труб И.И., Объектно-ориентированное моделирование на C++: Учебный курс. - СПб.: Питер, 2006
+--
+-- A large discount house is planning to install a system to control the inventory of
+-- a particular radio. The time between demands for a radio is exponentially distributed
+-- with a mean time of 0.2 weeks. In the case where customers demand the radio when it
+-- is not in stock, 80 percent will go to another nearby discount house to find it, thereby
+-- representing lost sales, while the other 20 percent will backorder the radio and wait
+-- for the next shipment arrival. The store employs a periodic review-reorder point
+-- inventory system where the inventory status is reviewed every four weeks to decide if
+-- an order should be placed. The company policy is to order up to the stock control level
+-- of 72 radios whenever the inventory position, consisting of the radios in stock plus
+-- the radios on order minus the radios on backorder, is found to be less than or equal to
+-- the reorder point of 18 radios. The procurement lead time (the time from the placement
+-- of an order to its receipt) is constant and requires three weeks.
+--
+-- The objective of this example is to simulate the inventory system for a period of six
+-- years (312 weeks) to obtain statistics on the following quantities:
+--
+--   1) number of radios in stock;
+--   2) inventory position;
+--   3) safety stock (radios in stock at order receipt times); and
+--   4) time between lost sales.
+--
+-- The initial conditions for the simulation are an inventory position of 72 and no
+-- initial backorders. In order to reduce the bias in the statistics due to the initial
+-- starting conditions, all the statistics are to be cleared at the end of the first year
+-- of the six year simulation period.
 
 import Control.Monad
 import Control.Monad.Trans
