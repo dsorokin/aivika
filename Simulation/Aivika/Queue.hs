5a2f336ea2c33c84c2087e6942b5f46d0a1ab704
David Sorokin <david.sorokin@gmail.com>
2015-06-27 11:17:39 +0300

    Fixed comment.

--- Simulation/Aivika/Queue.hs
+++ Simulation/Aivika/Queue.hs
@@ -487,7 +487,7 @@ queueWaitTimeChanged_ q =
 -- | Return the total wait time from the time at which the enqueueing operation
 -- was initiated to the time at which the item was dequeued.
 --
--- In some sense, @queueTotalWaitTime == queueInputWaitTime + queueWaitTime@.
+-- In some sense, @queueTotalWaitTime == enqueueWaitTime + queueWaitTime@.
 --
 -- See also 'queueTotalWaitTimeChanged' and 'queueTotalWaitTimeChanged_'.
 queueTotalWaitTime :: Queue si sm so a -> Event (SamplingStats Double)
