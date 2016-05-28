d68ae8f6fb6bcb6b9144157835c15a09547de769
David Sorokin <david.sorokin@gmail.com>
2016-05-28 17:59:52 +0300

    Updated the copyright year.

--- Simulation/Aivika/Queue.hs
+++ Simulation/Aivika/Queue.hs
@@ -1,7 +1,7 @@
 
 -- |
 -- Module     : Simulation.Aivika.Queue
--- Copyright  : Copyright (c) 2009-2015, David Sorokin <david.sorokin@gmail.com>
+-- Copyright  : Copyright (c) 2009-2016, David Sorokin <david.sorokin@gmail.com>
 -- License    : BSD3
 -- Maintainer : David Sorokin <david.sorokin@gmail.com>
 -- Stability  : experimental
