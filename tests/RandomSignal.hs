9f08266959a2f52ac07306a0a8ec31254af03c5b
David Sorokin <david.sorokin@gmail.com>
2016-12-03 10:26:18 +0300

    Added a test.

---
+++ tests/RandomSignal.hs
@@ -0,0 +1,31 @@
+
+import Control.Monad
+import Control.Monad.Trans
+
+import Simulation.Aivika
+
+specs = Specs 0 10 0.01 RungeKutta4 SimpleGenerator
+
+model :: Simulation Results
+model =
+  do x <- runCompositeInStartTime_ $
+          newRandomExponentialSignal 0.5
+
+     timer <- newArrivalTimer
+
+     let y = traceSignal "Y" $
+             arrivalTimerSignal timer $
+             delaySignal 1 $
+             traceSignal "X" x
+
+     runCompositeInStartTime_ $
+       sinkSignal y
+
+     return $
+       results
+       [resultSource "timer" "the arrival timer (EX ~ 0.5)" timer]
+
+main =
+  printSimulationResultsInStopTime
+  printResultSourceInRussian
+  model specs
